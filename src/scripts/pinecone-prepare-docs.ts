10cb9655f7b211266f5a14d4d011c9a305c5897c
Avijit Hazra <avijithazrabwn@gmail.com>
2024-12-15 13:23:25 +0530

    debugging on upload/route.ts

--- src/scripts/pinecone-prepare-docs.ts
+++ src/scripts/pinecone-prepare-docs.ts
@@ -6,16 +6,16 @@ import fs from "fs"
 // more time to init, so give some 5 mins after index
 // creation and try again.
 
- (async () => {
-  try {
-    const pineconeClient = await getPineconeClient();
-    console.log("Preparing chunks from PDF file");
-    const docs = await getChunkedDocsFromPDF();
-    fs.writeFileSync("docs.json",JSON.stringify(docs,null,2))
-    console.log(`Loading ${docs.length} chunks into pinecone...`);
-    await embedAndStoreDocs(pineconeClient, docs);
-    console.log("Data embedded and stored in pine-cone index");
-  } catch (error) {
-    console.error("Init client script failed ", error);
-  }
-})();
+//  (async () => {
+//   try {
+//     const pineconeClient = await getPineconeClient();
+//     console.log("Preparing chunks from PDF file");
+//     const docs = await getChunkedDocsFromPDF();
+//     fs.writeFileSync("docs.json",JSON.stringify(docs,null,2))
+//     console.log(`Loading ${docs.length} chunks into pinecone...`);
+//     await embedAndStoreDocs(pineconeClient, docs);
+//     console.log("Data embedded and stored in pine-cone index");
+//   } catch (error) {
+//     console.error("Init client script failed ", error);
+//   }
+// })();
