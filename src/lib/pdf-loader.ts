e074c5c341a3d5a850e2030426201b27b659f84e
Avijit Hazra <avijithazrabwn@gmail.com>
2024-12-15 13:19:55 +0530

    debugging on upload/route.ts

--- src/lib/pdf-loader.ts
+++ src/lib/pdf-loader.ts
@@ -1,24 +1,24 @@
-import "pdf-parse"; // Peer dep
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { env } from "./config";
+// import "pdf-parse"; // Peer dep
+// import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
+// import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+// import { env } from "./config";
 
 export async function getChunkedDocsFromPDF() {
   try {
     throw Error('\n\n====================== pdf loader path================================\n'+process.cwd()+'\n====================== pdf loader path================================\n\n')
 
-    const loader = new PDFLoader(env.PDF_PATH);
-    const docs = await loader.load();
+    // const loader = new PDFLoader(env.PDF_PATH);
+    // const docs = await loader.load();
 
-    // From the docs https://www.pinecone.io/learn/chunking-strategies/
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 1000,
-      chunkOverlap: 200,
-    });
+    // // From the docs https://www.pinecone.io/learn/chunking-strategies/
+    // const textSplitter = new RecursiveCharacterTextSplitter({
+    //   chunkSize: 1000,
+    //   chunkOverlap: 200,
+    // });
 
-    const chunkedDocs = await textSplitter.splitDocuments(docs);
+    // const chunkedDocs = await textSplitter.splitDocuments(docs);
 
-    return chunkedDocs;
+    // return chunkedDocs;
   } catch (e) {
     console.error(e);
     throw new Error("PDF docs chunking failed !");
